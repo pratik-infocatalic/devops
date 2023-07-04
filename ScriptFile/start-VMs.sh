6587958b643fbb1366f58d79d3b068a20629dcce
root <root@instance-5.asia-south1-c.c.forward-liberty-389906.internal>
2023-07-04 22:52:54 +0530

    instance-5 added script file,now all files are together

--- ScriptFile/start-VMs.sh
+++ ScriptFile/start-VMs.sh
@@ -9,9 +9,11 @@ function startVM()
 		gcloud config set compute/zone asia-south1-c
 		gcloud compute instances start instance-7
 		gcloud compute instances start ubuntu-2
+		gcloud compute instances start instance-5
 		gcloud config set compute/zone us-west4-b
 		gcloud compute instances start instance-4
 		gcloud compute instances start instance-2
+
                 
 	else
 		echo "something happen wrong"
--- ScriptFile/stop-VMs.sh
+++ ScriptFile/stop-VMs.sh
@@ -8,6 +8,7 @@ then
 gcloud config set compute/zone asia-south1-c
                 gcloud compute instances stop instance-7
                 gcloud compute instances stop ubuntu-2
+		gcloud compute instances stop instance-5
                 gcloud config set compute/zone us-west4-b
                 gcloud compute instances stop instance-4
                 gcloud compute instances stop instance-2
